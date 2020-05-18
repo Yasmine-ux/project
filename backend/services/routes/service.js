07770d3893c87f07983f8ff45c29622affc1d3a5
Hejer Damak <hejer.damak@gmail.com>
2020-05-18 19:13:08 +0100

    backend services done

--- backend/services/routes/service.js
+++ backend/services/routes/service.js
@@ -5,46 +5,48 @@ const Service = require('../models/collection')
 
 
 router.post("/add", (req,res) => {
-    const {name, category, description} = req.body
+    const {title, category, description} = req.body
     const newService = new Service({
-        name, category, description
+        title, category, description
     })
     newService.save()
-    .then(services => res.send(services))
-    .catch(err => console.log(err))
+    .then( data => res.json({ result: data }) )
+    .catch( err => res.json({ error: err }) )
 })
 
 
 router.get("/",(req,res) => {
     Service.find()
-    .then(services => res.send(services))
-    .catch(err => console.log(err))
+    .then( data => res.json({ result: data }) )
+    .catch( err => res.json({ error: err }) )
 })
 
 
 router.get("/:id",(req,res) => {
-    const {_id} = req.params
-    Service.findOne({_id})
-      .then(services => res.send(services))
-    .catch(err => console.log(err))
+    const {id} = req.params
+    Service.findOne({_id: id})
+      .then( data => res.json({ result: data }) )
+    .catch( err => res.json({ error: err }) )
 })
 
 
 
 router.delete("/remove/:id",(req,res) => {
-    const {_id} = req.params
-    Service.findOneAndDelete({_id:_id})
-    .then(services => res.send("success"))
-    .catch(err => console.log(err))
+    const {id} = req.params
+    Service.findOneAndDelete({_id: id})
+    .then( data => res.json({
+        result: { status: true, message: "user deleted successfuly" } }))
+    .catch( err => res.json({ error: err }) )
 })
 
 
 router.put("/update/:id",(req,res) => {
-    const {_id}=req.params
-    const {name, category, description}=req.body
-    Service.findOneAndUpdate({_id},{$set:{name, category, description}})
-    .then(Services => res.send("Service Updated"))
-    .catch(err => console.log(err))
+    const {id} = req.params
+    const {title, category, description} = req.body
+    Service.findOneAndUpdate({_id: id},{$set:{title, category, description}})
+    .then( data => res.json({
+        result: { status: true, message: "user updated successfuly" } }))
+    .catch( err => res.json({ error: err }) )
 })
 
 module.exports = router
